7c1d0ea0f34e5a78a99e9df63f272ae0346aa21b
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-07-31 14:53:07 -0700

    Create operate function Function runs other functions

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(a, operator, b) {
+    if (operator == "+") {
+        return add(a, b);
+    } else if (operator == "-") {
+        return subtract(a, b);
+    } else if (operator == "*") {
+        return multiply(a, b);
+    } else if (operator == "/") {
+        return divide(a, b);
+    }
+}
