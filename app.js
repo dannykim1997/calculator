a61601bcb395888d6971b0375cfa30407d70e3b7
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-08-04 17:52:46 -0700

    Calculate equation in console.log

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let displayValue = document.querySelector(".display-value");
 let displayBottom = document.querySelector(".display-bottom");
 let numButtons = document.querySelectorAll(".number");
 let expressButtons = document.querySelectorAll(".expression");
+let equalButton = document.querySelector(".equal");
 
 let number1 = document.createElement('div');
 let expression = document.createElement('div');
@@ -14,6 +15,11 @@ let partTwo = false;
 number1.textContent = "";
 expression.textContent = "";
 number2.textContent = "";
+let num1String;
+let num2String;
+let num1;
+let num2;
+let operator;
 
 
 
@@ -22,7 +28,6 @@ function displayEquation() {
     getNumberOne(); 
     getOperator();
     getNumberTwo();
-    operate();
 }
 displayEquation();
 
@@ -31,9 +36,11 @@ function getNumberOne() {
         if(partOne == true) {
         number1.textContent += e.target.innerHTML;
         displayBottom.appendChild(number1);
-        console.log('Default partOne true, partTwo false');
-        console.log(`getNumberOne partOne ${partOne}`);
-        console.log(`getNumberOne partTwo ${partTwo}`);
+        num1String = number1.textContent;
+        num1 = parseInt(num1String);
+        // console.log('Default partOne true, partTwo false');
+        // console.log(`getNumberOne partOne ${partOne}`);
+        // console.log(`getNumberOne partTwo ${partTwo}`);
         }
     }))
 }
@@ -43,10 +50,11 @@ function getOperator() {
         expression.textContent = e.target.innerHTML;
         displayTop.appendChild(number1);        
         displayTop.appendChild(expression);
+        operator = expression.textContent;
         partOne = false;
         partTwo = true;
-        console.log(`getOperator partOne ${partOne}`);
-        console.log(`getOperator partTwo ${partTwo}`);      
+        // console.log(`getOperator partOne ${partOne}`);
+        // console.log(`getOperator partTwo ${partTwo}`);      
     })))
 }
 
@@ -55,14 +63,31 @@ function getNumberTwo() {
         if(partTwo == true) {
         number2.textContent += e.target.innerHTML;
         displayBottom.appendChild(number2);
-        console.log(`getNumberTwo partOne ${partOne}`);
-        console.log(`getNumberTwo partTwo ${partTwo}`);
+        num2String = number2.textContent;
+        num2 = parseInt(num2String);
+        // console.log(`getNumberTwo partOne ${partOne}`);
+        // console.log(`getNumberTwo partTwo ${partTwo}`);
         }
     }))
 }
 
-function operate() {
-    console.log(number1);
+function clickEqualButton() {
+    equalButton.addEventListener('click', () => {
+        doOperation();
+    });
+}
+clickEqualButton();
+
+function doOperation() {
+    if(operator == '+') {
+        console.log(num1+num2);
+    } else if(operator == '-') {
+        console.log(num1-num2);
+    } else if(operator == '*') {
+        console.log(num1*num2);
+    } else if(operator == '/') {
+        console.log(num1/num2);
+    }
 }
 
 function add(num1, num2) {
@@ -81,14 +106,14 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, operator, num2) {
-    if (operator == "+") {
-        return add(num1, num2);
-    } else if (operator == "-") {
-        return subtract(num1, num2);
-    } else if (operator == "*") {
-        return multiply(num1, num2);
-    } else if (operator == "/") {
-        return divide(num1, num2);
-    }
-}
+// function operate(num1, operator, num2) {
+//     if (operator == "+") {
+//         return add(num1, num2);
+//     } else if (operator == "-") {
+//         return subtract(num1, num2);
+//     } else if (operator == "*") {
+//         return multiply(num1, num2);
+//     } else if (operator == "/") {
+//         return divide(num1, num2);
+//     }
+// }
